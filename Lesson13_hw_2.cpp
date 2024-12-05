f727297111fe45785d07d87f148f21358427468e
Rodion <roksis72010@gmail.com>
2024-12-05 15:03:26 +0100

    Add project files.

---
+++ Lesson13_hw_2.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <iostream>
+#include <stack>
+using namespace std;
+int PolishRecord(const string& line) {
+	stack<int> num;
+	for (int i = 0; i < line.size(); i++)
+	{
+		if (isdigit(line[i])) { num.push(line[i] - '0'); }
+		else 
+		{
+			int b = num.top();
+			num.pop();
+			int a = num.top();
+			num.pop();
+			if (line[i] == '+') {
+				num.push(a + b);
+			}
+			else if (line[i] == '-')
+			{
+				num.push(a - b);
+			}
+			else if (line[i] == '*')
+			{
+				num.push(a * b);
+			}
+			else if (line[i] == '/')
+			{
+				num.push(a / b);
+			}
+		}
+	}
+
+	return num.top();
+}
+int main()
+{
+	cout << PolishRecord("825*+132*+4-/") << endl;
+}
